e2c57105e0c006b245bff7ea67a13d1e0aa069ce
SangUk Park <parksang1993@gmail.com>
2024-08-25 15:57:19 +0900

    Initial Commit

---
+++ Board/BoardApp.swift
@@ -0,0 +1,34 @@
+//
+//  BoardApp.swift
+//  Board
+//
+//  Created by 박상욱 on 8/25/24.
+//
+
+import SwiftUI
+
+@main
+struct BoardApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+        .windowStyle(.volumetric)
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+    }
+}
---
+++ BoardTests/BoardTests.swift
@@ -0,0 +1,17 @@
+//
+//  BoardTests.swift
+//  BoardTests
+//
+//  Created by 박상욱 on 8/25/24.
+//
+
+import Testing
+@testable import Board
+
+struct BoardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
